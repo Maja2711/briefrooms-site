0b70fac05c81834ef16d06d0528454491674366e
Maja2711 <szczerbinski0577@wp.pl>
2026-08-01 10:23:12 +0200

    fix(investments): bust weekly integrity asset cache

--- scripts/render_weekly_public_pages.py
+++ scripts/render_weekly_public_pages.py
@@ -9,7 +9,7 @@ from zoneinfo import ZoneInfo
 
 ROOT = Path(__file__).resolve().parents[1]
 HEADER_VERSION = "20260719-1"
-ASSET_VERSION = "governance-12"
+ASSET_VERSION = "integrity-13"
 TZ = ZoneInfo("Europe/Warsaw")
 
 PAGES = {
